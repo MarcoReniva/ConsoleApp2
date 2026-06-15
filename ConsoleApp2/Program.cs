4a5b5f413632b71a1f22d7e6527b3f718459bf91
MarcoReniva <25-0009c@sgen.edu.ph>
2026-06-15 21:26:53 +0800

    Updated Filip Hula Program.cs

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,41 +1,56 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Net;
 
 namespace ConsoleApp2
 {
     internal class Program
     {
+        static string currentUser = "";
         static void Main(string[] args)
         {
-            menu();
-            
+            mainmenu();
+
         }
-        static void menu()
+        static void mainmenu()
         {
-            Console.WriteLine("=====================================");
-            Console.WriteLine("        Welcome to FILIPI-HULA         ");
-            Console.WriteLine("=====================================");
-            Console.WriteLine("[1].Login");
-            Console.WriteLine("[2].Register");
-            Console.WriteLine("[3].Exit");
-            Console.Write("choose an option: ");
-            string output = Console.ReadLine();
-            switch(output)
+            bool try2 = false;
+            while(!try2)
             {
-                case "1":
-                    login();
-                    break;
-                case "2":
-                    regist();
-                    break;
-                case "3":
-                    Console.WriteLine("Thank you for Playing!");
-                    break;
+                Console.WriteLine("=====================================");
+                Console.WriteLine("        Welcome to FILIPI-HULA         ");
+                Console.WriteLine("=====================================");
+                Console.WriteLine("[A].Login");
+                Console.WriteLine("[B].Register");
+                Console.WriteLine("[C].Exit");
+                Console.Write("choose an option: ");
+                string output = Console.ReadLine().ToUpper();
+                switch (output)
+                {
+                    case "A":
+                        login();
+                        try2=true;
+                        break;
+                    case "B":
+                        regist();
+                        try2 = true;
+                        break;
+                    case "C":
+                        Console.WriteLine("Thank you for Playing!");
+                        try2 = true;
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Invalid Option pls enter either A, B or C");
+                        try2 = false;
+                        break;
+                }
             }
+           
         }
         static void login()
         {
@@ -44,35 +59,114 @@ namespace ConsoleApp2
             Console.Write("Enter Password: ");
             string pass = Console.ReadLine();
             string[] lines2 = File.ReadAllLines("login.txt");
-            if (!lines2.Contains(usern))
+            if (usern == "" || pass == "")
             {
-                Console.WriteLine("No accounts found. Please register first.");
-                menu();
+                Console.Clear();
+                Console.WriteLine("Username and Password cannot be empty");
                 return;
             }
-
+            if (!lines2.Contains(usern))
+            {
+                Console.Clear();
+                Console.WriteLine("No accounts found. Please register first.");
+                mainmenu();
+                return;
+            }
             string[] lines = File.ReadAllLines("login.txt");
-
             for (int i = 0; i + 1 < lines.Length; i += 2)
             {
+
                 if (lines[i] == usern && lines[i + 1] == pass)
                 {
+                    Console.Clear();
+                    currentUser = usern;
                     Console.WriteLine("Login successful! Welcome, " + usern + "!");
-                    return;
+                    gamemenu();
                 }
             }
-
-            Console.WriteLine("Invalid username or password. Try again.");
-            menu();
+           Console.WriteLine("Invalid username or password. Try again.");
+           mainmenu();
+            
         }
         static void regist()
         {
-            Console.Write("Enter new username: ");
-            string newuser = Console.ReadLine();
-            Console.WriteLine("Enter new password");
-            string newpass = Console.ReadLine();
-            string[]newaccount = {newuser, newpass };
-            File.AppendAllLines("login.txt", newaccount);
+            bool regis = false;
+            Console.Clear();
+            while (!regis)
+            {
+                Console.WriteLine("========Account Registration=====");
+                Console.Write("Enter new username: ");
+                string newuser = Console.ReadLine();
+                Console.Write("Enter new password: ");
+                string newpass = Console.ReadLine();
+                Console.Write("Confirm Password: ");
+                string confpass = Console.ReadLine();
+                Console.WriteLine("=================================");
+                if (newuser.Trim() == "" || newpass.Trim() == "" || confpass.Trim() == "")
+                {
+                    Console.Clear();
+                    Console.WriteLine("Username and Password cannot be empty!");
+                    continue;
+                }
+                if (confpass != newpass)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Password does not match!");
+                    continue;
+                }
+                if (File.Exists("login.txt") && File.ReadAllText("login.txt").Contains(newuser + ","))
+                {
+                    Console.Clear();
+                    Console.WriteLine("Account already exists!");
+                    continue;
+                }
+                else
+                {
+                    string[] newaccount = { newuser + "," + confpass };
+                    File.AppendAllLines("login.txt", newaccount);
+                    Console.WriteLine("Registration Successfull!");
+                    break;
+                }
+            }
         }
+        static void gamemenu()
+        {
+            bool try3 = false; 
+            while (!try3)
+            {
+                Console.WriteLine("Playing as: " + currentUser);
+                Console.WriteLine("=====================================");
+                Console.WriteLine("        Welcome to FILIPI-HULA         ");
+                Console.WriteLine("=====================================");
+                Console.WriteLine("Choose your gamemode");
+                Console.WriteLine("[A]. Hangman");
+                Console.WriteLine("[B]. Reading Comprehension");
+                Console.WriteLine("[C]. Go back");
+                Console.Write("Enter choice: ");
+                string output2 = Console.ReadLine().ToUpper();
+                switch (output2)
+                {
+                    case "A":
+                        try3 = true;
+                        break;
+                    case "B":
+                        try3 = true;
+                        break;
+                    case "C":
+                        currentUser = "";
+                        try3 = true;
+                        Console.Clear();
+                        mainmenu();
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Invalid Option pls enter either A, B or C");
+                        try3 = false;
+                        break;
+                }
+            }
+            
+        }
+
     }
 }
